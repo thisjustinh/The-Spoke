0153dfcfe9c1c9dc8f15717918c67633fdcd2187
Justin Huang <xinglongjh@gmail.com>
2018-11-28 14:19:50 -0500

    Create issue.php

---
+++ December Issue/issue.php
@@ -0,0 +1,32 @@
+<?php /* Template Name: Issue */ ?>
+
+<!DOCTYPE html>
+
+<!-- Made by Justin Huang and Melinda Xu
+
+ooooooooooooo oooo                        .oooooo..o ooooooooo.     .oooooo.   oooo    oooo oooooooooooo 
+8'   888   `8 `888                       d8P'    `Y8 `888   `Y88.  d8P'  `Y8b  `888   .8P'  `888'     `8 
+     888       888 .oo.    .ooooo.       Y88bo.       888   .d88' 888      888  888  d8'     888         
+     888       888P"Y88b  d88' `88b       `"Y8888o.   888ooo88P'  888      888  88888[       888oooo8    
+     888       888   888  888ooo888           `"Y88b  888         888      888  888`88b.     888    "    
+     888       888   888  888    .o      oo     .d8P  888         `88b    d88'  888  `88b.   888       o 
+    o888o     o888o o888o `Y8bod8P'      8""88888P'  o888o         `Y8bood8P'  o888o  o888o o888ooooood8
+
+...and special thanks to Stack Overflow -->
+
+    <head>
+        <title>The Spoke - December Issue</title>
+        <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro|Work+Sans|Montserrat:700" rel="stylesheet">
+        <link rel="stylesheet" href="http://spoke.news/wp-content/themes/newsfront/issue/issue.css">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    </head>
+
+    <body>
+        <div id = container1>
+            
+            <a href = "#top"><button id = "myBtn" title = "Go To Top">Top</button></a>
+            <ul id = "#top">
+                <li><a href = "http://spoke.news/"><img src = "http://spoke.news/wp-content/themes/newsfront/candidates/spokelogo2.png" width = "180" height = "32"></a></li>
+            </ul>
+        </div>
+    </body>
